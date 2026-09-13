caf5f7d8e4aad705b7a580d7d9a8c758ccb4765b
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-13 19:52:45 +0700

    week01: (tugas 2) implement discount logic

--- src/week01/GameStore.kt
+++ src/week01/GameStore.kt
@@ -4,3 +4,5 @@ fun main() {
     val gameTitle = "No Man's Sky"
     val price = 449999
 }
+
+fun calculateDiscount(price: Int) if (price > 500000) price - (price - 20/100) else price - (price - 10/100)
