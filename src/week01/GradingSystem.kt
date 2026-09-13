074794c3364729a8e5e2cdca77db338d4460a55f
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-13 17:37:09 +0700

    week01: implement null safety check

--- src/week01/GradingSystem.kt
+++ src/week01/GradingSystem.kt
@@ -16,6 +16,12 @@ fun main() {
     println("Grade kamu: $grade")
 
     println("Status: ${calculateStatus(score)}")
+
+    val studentId: String? = null
+
+    val idLenght = studentId?.length ?: 0
+
+    println("Panjang ID: $idLenght")
 }
 
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
