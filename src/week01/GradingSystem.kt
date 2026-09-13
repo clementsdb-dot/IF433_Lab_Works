ebac830ef51e8fde64c9570f219a4e4672406aa2
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-13 17:36:05 +0700

    week01: add calculateStatus function

--- src/week01/GradingSystem.kt
+++ src/week01/GradingSystem.kt
@@ -14,6 +14,9 @@ fun main() {
     }
 
     println("Grade kamu: $grade")
+
+    println("Status: ${calculateStatus(score)}")
 }
 
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
 
