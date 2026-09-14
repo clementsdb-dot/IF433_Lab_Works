e047b0b88019acd462de68fa9cba998997071b0f
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-14 20:21:56 +0700

    week02: create basic Student class structure

---
+++ src/week02/Main.kt
@@ -0,0 +1,27 @@
+package oop_01_ClementsDB.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Name: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karaketer")
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran selesai.")
+    }
+}
