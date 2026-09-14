2764c96d427fce7a5004e0c362a2b41057f0c37d
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-14 20:24:19 +0700

    week02: add warning validation in init block

--- src/week02/Students.kt
+++ src/week02/Students.kt
@@ -1,2 +1,12 @@
 package oop_01_ClementsDB.week02
 
+class Student(val name: String, val nim: String, val major: String) {
+    init {
+        if(nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+}
