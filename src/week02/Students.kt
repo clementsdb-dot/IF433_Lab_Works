398b2cc42b4ad180ce803c10c0516a01ba1ca2cc
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-16 22:50:01 +0700

    week02: add secondary constructor

--- src/week02/Students.kt
+++ src/week02/Students.kt
@@ -10,3 +10,7 @@ class Student(val name: String, val nim: String, val major: String) {
         }
     }
 }
+
+(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+    println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+}
