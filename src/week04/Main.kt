f677fbf33cb6fd00d45c6c1e01e2898e90ceeac5
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-19 10:31:50 +0700

    week04: test Vehicle and Car in main

---
+++ src/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_01_ClementsDB.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle()
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
--- src/week04/Vehicle.kt
+++ src/week04/Vehicle.kt
@@ -1,6 +1,8 @@
 package oop_01_ClementsDB.week04
 
 open class Vehicle(val brand: String) {
+    constructor() : this()
+
     var speed: Int = 0
 
     open fun accelerate() {
