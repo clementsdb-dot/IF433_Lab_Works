508de0951e7d05cf33c738832feac4d0186e31ce
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-19 21:20:54 +0700

    week03: fix setter using backing field

--- src/oop_140826_ClementsDB/week03/Employee.kt
+++ src/oop_140826_ClementsDB/week03/Employee.kt
@@ -2,6 +2,12 @@ package oop_140826_ClementsDB.week03
 
 class Employee(val name: String ) {
     var salary: Int = 0
+        set(value)
+            if(value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+            } else {
+                field = value
+            }
 }
 
 var salary: Int = 0
