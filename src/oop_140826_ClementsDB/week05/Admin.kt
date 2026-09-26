5ffa2c78fde72997f1721d59a43d7041a2515c05
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-26 10:20:13 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/oop_140826_ClementsDB/week05/Admin.kt
@@ -0,0 +1,11 @@
+package oop_140826_ClementsDB.week05
+
+class Admin {nama: String : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi")
+    }
+
+    fun doAdminWork() {
+        println("[$name] sedang merekap data absensi mahasiswa.")
+    }
+}
