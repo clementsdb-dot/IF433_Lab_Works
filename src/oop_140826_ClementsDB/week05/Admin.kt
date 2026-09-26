2794747da52dc431ae95b6a19ea1196b011e6911
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-26 10:29:41 +0700

    week05: implement polymorphic collection and smart casting

--- src/oop_140826_ClementsDB/week05/Admin.kt
+++ src/oop_140826_ClementsDB/week05/Admin.kt
@@ -1,11 +1,11 @@
 package oop_140826_ClementsDB.week05
 
-class Admin {nama: String : Pegawai(nama) {
+class Admin (nama: String) : Pegawai(nama) {
     override fun bekerja() {
         println("[$nama] sedang duduk di depan komputer melayani administrasi")
     }
 
     fun doAdminWork() {
-        println("[$name] sedang merekap data absensi mahasiswa.")
+        println("[$nama] sedang merekap data absensi mahasiswa.")
     }
 }
