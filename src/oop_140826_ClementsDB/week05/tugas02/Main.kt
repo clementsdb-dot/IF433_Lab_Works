3dc597ff225c927a9de94ff32df187d2fe823f6f
Clements Dymasius Budiargo <clements.dymasius.budiargo@gmail.com>
2026-09-26 15:04:07 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_140826_ClementsDB/week05/tugas02/Main.kt
+++ src/oop_140826_ClementsDB/week05/tugas02/Main.kt
@@ -1,2 +1,13 @@
 package oop_140826_ClementsDB.week05.tugas02
 
+fun main(){
+    val ewallet = EWallet("AbrahamMelanggarHAM", 500000.0)
+    val card = CreditCard("VitoBoluLapisPamulang", 100000.0)
+
+    val caraPembayaran: List<PaymentMethod> = listOf(ewallet, card)
+
+    println("--- Pembayaran ---")
+    for (bayar in caraPembayaran) {
+        bayar.processPayment(75000.0)
+    }
+}
